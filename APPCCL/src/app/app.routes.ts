082cb8e87ca50fc1c5ae95e64e0ade23c63098a1
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-02-15 09:56:41 -0500

    configuracion de Routes: direccionamiento entre las paginas.

--- APPCCL/src/app/app.routes.ts
+++ APPCCL/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { ProductosComponent } from './Pages/productos/productos.component';
+import { NuevoComponent } from './Pages/nuevo/nuevo.component';
+import { LoginComponent } from './Pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:InicioComponent},
+    {path:'inicio',component:InicioComponent},
+    {path:'nuevo',component:NuevoComponent},
+    {path:'login',component:LoginComponent},
+    {path:'productos/:id',component:ProductosComponent},
+    
+];
