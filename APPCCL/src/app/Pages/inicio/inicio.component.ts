d88a45151104426ace1c4ac1b1d96b304ec28ed1
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-02-15 10:34:49 -0500

    pagina Html y TS de visualizacion de inventario.

--- APPCCL/src/app/Pages/inicio/inicio.component.ts
+++ APPCCL/src/app/Pages/inicio/inicio.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { Productos } from '../../Models/Productos';
+import { ProductosService } from '../../Services/productos.service';
+import { Router } from '@angular/router';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSortModule
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  
+  private productosService = inject(ProductosService);
+  public listaProductos: MatTableDataSource<Productos> = new MatTableDataSource<Productos>();
+  public displayedColumns: string[] = [
+    'id',
+    'nombre',
+    'cantidad',
+  ];
+  
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.listadoProductos();
+
+    console.log('S1', this.listaProductos);
+  }
+
+  listadoProductos() {
+    
+    this.productosService.lista().subscribe({
+      next: (res) => {
+        console.log('S1', this.listaProductos);
+        console.log(res);
+        if (res && res.data) {
+          this.listaProductos.data = res.data; 
+        }
+      },
+      error: (err) => {
+        console.log(err.message);
+      },
+    });
+  }
+
+  nuevo(id: string) {
+    this.router.navigate(['/nuevo'], { queryParams: { id: id ?? '0' } });
+  }
+  ver(name: string) {
+    this.router.navigate(['/ver'], { queryParams: { username: name } });
+  }
 
 }
